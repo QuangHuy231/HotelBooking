2fcf84508118cbee96944f63be469a7d79d46abd
Quang Huy <nguyenquanghuya3kd@gmail.com>
2023-03-14 21:14:28 +0700

    detailPage

--- frontend/src/page/DetailPlacePage.jsx
+++ frontend/src/page/DetailPlacePage.jsx
@@ -1,6 +1,8 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import BookingWidget from "../components/BookingWidget";
+import PlaceGallery from "../components/PlaceGallery";
 
 const DetailPlacePage = () => {
   const { id } = useParams();
@@ -9,8 +11,8 @@ const DetailPlacePage = () => {
     if (!id) {
       return;
     }
-    axios.get(`/place/${id}`).then((response) => {
-      setPlace(response.data);
+    axios.get(`/place/${id}`).then(({ data }) => {
+      setPlace(data);
     });
   }, [id]);
 
@@ -19,8 +21,34 @@ const DetailPlacePage = () => {
   return (
     <div className="mt-4 bg-gray-100 -mx-8 px-8 pt-8">
       <h1 className="text-3xl">{place.title}</h1>
-      <h2>{place.address}</h2>
+      <a
+        target="_blank"
+        href={`https://maps.google.com/?q=${place.address}`}
+        className="flex gap-1 my-3 block font-semibold underline"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z"
+          />
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z"
+          />
+        </svg>
 
+        {place.address}
+      </a>
+      <PlaceGallery place={place} />
       <div className="mt-8 mb-8 grid gap-8 grid-cols-1 md:grid-cols-[2fr_1fr]">
         <div>
           <div className="my-4">
@@ -33,7 +61,9 @@ const DetailPlacePage = () => {
           <br />
           Max number of guests: {place.maxGuests}
         </div>
-        <div></div>
+        <div>
+          <BookingWidget place={place} />
+        </div>
       </div>
       <div className="bg-white -mx-8 px-8 py-8 border-t">
         <div>
